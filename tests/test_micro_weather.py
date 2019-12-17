04283e3e9848fe7c6417985de4128b543ad1f993
@mst <masato.u20@gmailcom>
2019-12-17 14:07:13 +0900

    update testcase for wrong previous commit

--- tests/test_micro_weather.py
+++ tests/test_micro_weather.py
@@ -10,9 +10,9 @@ import micro_weather as weather
 
 class TestMicroWeather(unittest.TestCase):
 
-    def test_get_weather_data_normal_01(self):
+    def test_call_weather_api_normal_01(self):
         url = settings.BASE_URL + 'id=' + settings.CITY_ID + '&APPID=' + settings.API_KEY
-        data  = weather.get_weather_data(url)
+        data  = weather.call_weather_api(url)
 
         self.assertTrue(type(data), type(""))
 
@@ -32,23 +32,22 @@ class TestMicroWeather(unittest.TestCase):
         self.assertTrue('humidity' in json_data['main'])
         self.assertTrue('speed' in json_data['wind'])
 
-    def test_get_weather_data_wrong_01(self):
 
-        wrong_url = "temp.com"
-        with self.assertRaises(Exception):
-            weather.get_weather_data(wrong_url)
+    def test_call_weather_api_wrong_01(self):
+
+        wrong_url = "https://www.not_exist.com/"
+        self.assertEqual(weather.call_weather_api(wrong_url), "")
 
         wrong_CITY_ID = '9999'
         url = settings.BASE_URL + 'id=' + wrong_CITY_ID + '&APPID=' + settings.API_KEY
-        data  = weather.get_weather_data(url)
-        self.assertEqual(data, "")
+        self.assertEqual(weather.call_weather_api(url), "")
 
         wrong_api_key = "xxx"
         url = settings.BASE_URL + 'id=' + settings.CITY_ID + '&APPID=' + wrong_api_key
-        data  = weather.get_weather_data(url)
-        print(data)
+        self.assertEqual(weather.call_weather_api(url), "")
 
-    def test_get_weather_data_csv_01(self):
+
+    def test_get_weather_data_01(self):
         json_data = '{"coord":{"lon":139.69,"lat":35.69}, \
                     "weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}], \
                     "base":"stations","main":{"temp":278.04,"feels_like":272.91,"temp_min":274.82,"temp_max":282.59, \
@@ -56,9 +55,9 @@ class TestMicroWeather(unittest.TestCase):
                     "dt":1576424667,"sys":{"type":1,"id":8074,"country":"JP","sunrise":1576446234,"sunset":1576481352}, \
                     "timezone":32400,"id":1850147,"name":"Tokyo","cod":200}'
 
-        expected = (1576424667, "Tokyo", 1850147, "Clouds", "scattered clouds", 278.04, 61, 4.1)
+        expected = [1576424667, "Tokyo", 1850147, "Clouds", "scattered clouds", 278.04, 61, 4.1]
 
-        self.assertEqual(weather.get_weather_data_csv(json_data), expected)
+        self.assertEqual(weather.get_weather_data(json_data), expected)
 
 
     def test_write_weather_data_to_csv_01(self):
